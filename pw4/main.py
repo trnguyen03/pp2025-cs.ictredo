3c91261504a947aba0ef3b6be75de2f10a0c2084
Dang Trung Nguyen <154598240+trnguyen03@users.noreply.github.com>
2024-12-23 21:01:42 +0700

    Update main.py

--- pw4/main.py
+++ pw4/main.py
@@ -1,27 +1,46 @@
 from input import input_students, input_courses, input_marks
-from output import display_students_curses
-from domains.student import Student
-from domains.course import Course
+from output import curses_
 from domains.management import StudentMarkManagement
 
 def main():
     management = StudentMarkManagement()
 
-    raw_students = input_students()
-    students = [Student(s_id, name, dob) for s_id, name, dob in raw_students]
-    management.add_students(students)
+    num_students = int(input("Enter number of students: "))
+    input_students(management, num_students)
 
-    raw_courses = input_courses()
-    courses = [Course(c_id, name, credit) for c_id, name, credit in raw_courses]
-    management.add_courses(courses)
+    num_courses = int(input("\nEnter number of courses: "))
+    input_courses(management, num_courses)
+    while True:
+        print("\nMenu:")
+        print("1. List Courses")
+        print("2. List Students")
+        print("3. Input Marks")
+        print("4. Show GPA (Curses UI)")
+        print("5. Exit")
+        choice = input("Choose an option: ")
 
-    marks = input_marks(raw_students, raw_courses)
-    management.add_marks(marks)
+        if choice == "1":
+            print("\nList of Courses:")
+            for course in management.get_courses():
+                print(course)
 
-    management.calculate_gpas()
-    management.sort_students_by_gpa()
+        elif choice == "2":
+            print("\nList of Students:")
+            for student in management.get_students():
+                print(student)
 
-    display_students_curses(management.students)
+        elif choice == "3":
+            input_marks(management)
+
+        elif choice == "4":
+            curses_display(management)
+
+        elif choice == "5":
+            print("Goodbyeee !")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
