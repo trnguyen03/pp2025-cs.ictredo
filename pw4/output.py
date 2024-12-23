844a810b15a35ecabd0cfbba9c2c9c3867ad505c
Dang Trung Nguyen <154598240+trnguyen03@users.noreply.github.com>
2024-12-23 20:57:10 +0700

    Update output.py

--- pw4/output.py
+++ pw4/output.py
@@ -1,12 +1,14 @@
 import curses
 
-def display_students_curses(students):
+def curses_display(management):
     def draw_menu(stdscr):
         stdscr.clear()
+        management.calculate_gpas()
+        management.sort_students_by_gpa()
         stdscr.addstr(0, 0, "Student List (Sorted by GPA):")
-        for idx, student in enumerate(students, start=1):
+        for idx, student in enumerate(management.get_students(), start=1):
             stdscr.addstr(idx, 0, str(student))
-        stdscr.addstr(len(students) + 2, 0, "Press any key to exit...")
+        stdscr.addstr(len(management.get_students()) + 2, 0, "Press any key to exit...")
         stdscr.refresh()
         stdscr.getch()
     
