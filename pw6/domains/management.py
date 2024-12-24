6ba3baf7fc86038cebcd34e4bc1ec4b3fbe208ca
Dang Trung Nguyen <154598240+trnguyen03@users.noreply.github.com>
2024-12-24 15:10:05 +0700

    Copy management.py from pw5

---
+++ pw6/domains/management.py
@@ -0,0 +1,30 @@
+from .student import Student
+from .course import Course
+
+class StudentMarkManagement:
+    def __init__(self):
+        self.students = []
+        self.courses = []
+
+    def add_students(self, student: Student):
+       self.students.append(student)
+
+    def get_students(self):
+        return self.students
+
+    def add_courses(self, course: Course):
+        self.courses.append(course)
+
+    def get_courses(self):
+        return self.courses
+        
+
+    def add_marks(self, marks):
+        self.marks = marks
+
+    def calculate_gpas(self):
+        for student in self.students:
+            student.calculate_gpa(self.courses)
+
+    def sort_students_by_gpa(self):
+        self.students.sort(key=lambda x: x.get_gpa(), reverse=True)
