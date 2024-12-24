56e411a8bb94749a3ec7f7142f62781c6437811b
Dang Trung Nguyen <154598240+trnguyen03@users.noreply.github.com>
2024-12-24 15:08:38 +0700

    Copy output.py from pw5

---
+++ pw6/output.py
@@ -0,0 +1,15 @@
+import curses
+
+def def curses_display(management):
+    def draw_menu(stdscr):
+        stdscr.clear()
+        management.calculate_gpas()
+        management.sort_students_by_gpa()
+        stdscr.addstr(0, 0, "Student List (Sorted by GPA):")
+        for idx, student in enumerate(management.get_students(), start=1):
+            stdscr.addstr(idx, 0, str(student))
+        stdscr.addstr(len(students) + 2, 0, "Press any key to exit...")
+        stdscr.refresh()
+        stdscr.getch()
+    
+    curses.wrapper(draw_menu)
