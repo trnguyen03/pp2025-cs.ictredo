7c287530131a4abb81501a03e325c2040a8edfee
Dang Trung Nguyen <154598240+trnguyen03@users.noreply.github.com>
2024-12-24 11:24:02 +0700

    Update input.py

--- pw5/input.py
+++ pw5/input.py
@@ -1,39 +1,56 @@
-def input_students():
-    students = []
-    num_students = int(input("Enter number of students: "))
+from domains.student import Student
+from domains.course import Course
+import os
+
+def input_students(management, num_students):
     for _ in range(num_students):
         student_id = input("Enter student ID: ")
         name = input("Enter student name: ")
         dob = input("Enter student Date of Birth (DD/MM/YYYY): ")
-        students.append((student_id, name, dob))
-    return students
+        students = Student((student_id, name, dob))
+        management.add_student(student)
+
+    write_students_to_file(management.get_students())
 
 
-def input_courses():
-    courses = []
-    num_courses = int(input("Enter number of courses: "))
+def input_courses(management, num_courses):
     for _ in range(num_courses):
         course_id = input("Enter course ID: ")
         name = input("Enter course name: ")
         credit = int(input("Enter course credit: "))
-        courses.append((course_id, name, credit))
-    return courses
+        course = Course(course_id, name, credit))
+        management.add_course(course)
+
+    write_courses_to_file(management.get_courses())
 
 
-def input_marks(students, courses):
-    marks = {}
-    for course_id, course_name, _ in courses:
-        marks[course_id] = {}
+def input_marks(management):
+    for course in management.get_courses():
         print(f"\nEntering marks for course: {course_name}")
-        for student_id, student_name, _ in students:
+        for student in management.get_students():
             while True:
                 try:
-                    mark = float(input(f"Enter marks for {student_name} (ID: {student_id}): "))
-                    if 0 <= mark <= 20:
-                        marks[course_id][student_id] = mark
-                        break
-                    else:
-                        print("Marks must be between 0 and 20.")
+                    mark = float(input(f"Enter marks for {student.get_name()} (ID: {student.get_id()}): "))
+                    student.add_mark(course.get_id(), mark)
+                    break
                 except ValueError:
                     print("Invalid input. Please enter a numeric value.")
-    return marks
+    write_marks_to_file(management.get_students())
+
+def write_students_to_file(students, filename="students.txt")
+
+    with open(filename, "w", encoding="utf-8") as f:
+        for student in students:
+            f.write("f{student.get_id()},{student.get_name()},{student.get_dob()}\n")
+
+def wrỉte_courses_to_file(courses, filenam="courses.txt"):
+    with open(filename, "w", encoding="utf-8") as f:
+        for course in courses:
+            f.write(f"{course.get_id()},{course.get_name()},{course.get_credit()}\n")
+
+def write_marks_to_file(students, filename="marks.txt"):
+    with open(filename, "w", encoding="utf-8") as f:
+        for student in students:
+            marks_dict = student.get_marks()
+            for course_id, mark in marks_dict.items():
+                f.write(f"{student.get_id()},{course_id},{mark}\n")
