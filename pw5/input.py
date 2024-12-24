b0d8844ebd5ae7d1aebc7995d897d2628f289601
Dang Trung Nguyen <154598240+trnguyen03@users.noreply.github.com>
2024-12-24 15:20:53 +0700

    Update input.py

--- pw5/input.py
+++ pw5/input.py
@@ -1,6 +1,5 @@
 from domains.student import Student
 from domains.course import Course
-import os
 
 def input_students(management, num_students):
     for _ in range(num_students):
@@ -10,7 +9,6 @@ def input_students(management, num_students):
         students = Student((student_id, name, dob))
         management.add_student(student)
 
-    write_students_to_file(management.get_students())
 
 
 def input_courses(management, num_courses):
@@ -21,12 +19,11 @@ def input_courses(management, num_courses):
         course = Course(course_id, name, credit))
         management.add_course(course)
 
-    write_courses_to_file(management.get_courses())
 
 
 def input_marks(management):
     for course in management.get_courses():
-        print(f"\nEntering marks for course: {course_name}")
+        print(f"\nEntering marks for course: {course.get_name()}")
         for student in management.get_students():
             while True:
                 try:
@@ -35,22 +32,5 @@ def input_marks(management):
                     break
                 except ValueError:
                     print("Invalid input. Please enter a numeric value.")
-    write_marks_to_file(management.get_students())
 
-def write_students_to_file(students, filename="students.txt")
 
-    with open(filename, "w", encoding="utf-8") as f:
-        for student in students:
-            f.write("f{student.get_id()},{student.get_name()},{student.get_dob()}\n")
-
-def wrỉte_courses_to_file(courses, filenam="courses.txt"):
-    with open(filename, "w", encoding="utf-8") as f:
-        for course in courses:
-            f.write(f"{course.get_id()},{course.get_name()},{course.get_credit()}\n")
-
-def write_marks_to_file(students, filename="marks.txt"):
-    with open(filename, "w", encoding="utf-8") as f:
-        for student in students:
-            marks_dict = student.get_marks()
-            for course_id, mark in marks_dict.items():
-                f.write(f"{student.get_id()},{course_id},{mark}\n")
